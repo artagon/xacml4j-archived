6183b2499eedad98ce427e391ec8d2d8dc189728
Rolandas Valteris <rolandas.valteris@gmail.com>
2014-03-05 17:25:21 -0500

    added xacml-rest module

---
+++ xacml-rest/docs/xacml-rest.mm
@@ -0,0 +1,18 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1393267955606" ID="ID_1765477737" MODIFIED="1393268306297" TEXT="xacml-rest">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1393268132881" HGAP="30" ID="ID_1126489649" MODIFIED="1393268284511" POSITION="right" TEXT="request kinds" VSHIFT="8">
+<font NAME="SansSerif" SIZE="14"/>
+<node CREATED="1393268147548" ID="ID_707891957" MODIFIED="1393268211062" TEXT="JSON+secirity">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1393268170910" ID="ID_1683146958" MODIFIED="1393268211060" TEXT="XACML+security">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node CREATED="1393268187046" ID="ID_520699070" MODIFIED="1393268211060" TEXT="XACML+SAML">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</map>
